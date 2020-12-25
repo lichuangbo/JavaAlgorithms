9e7dfc76e32db8e838471101caef797e86be630f
lcb <li-chuangbo@smtp.telek.com.cn>
2020-12-25 14:38:00 +0800

    字典树实现（添加，搜索，前缀搜索操作）

---
+++ src/top/xiaotian/dataStructures/trie/Trie.java
@@ -0,0 +1,137 @@
+package top.xiaotian.dataStructures.trie;
+
+import java.util.TreeMap;
+
+/**
+ * Trie字典树实现
+ * @author lichuangbo
+ * @email lichuangbo@smtp.telek.com.cn
+ * @time
+ * @Description: 描述:
+ */
+public class Trie {
+    private static class Node {
+
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            this.next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private final Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            // next指向为空，创建新的节点
+            if (curr.next.get(c) == null) {
+                curr.next.put(c, new Node());
+            }
+            // 不为空，向下走
+            curr = curr.next.get(c);
+        }
+        if (!curr.isWord) {// 这里的判断是为了维护size字段（只有他是新的单词结尾，才自增）
+            // 不一定就是叶子节点，这里标记他为一个单词的结尾：panda 和 pan
+            curr.isWord = true;
+            size++;
+        }
+    }
+
+    // 添加--递归写法
+    public void addR(String word) {
+        addR(root, word, 0);
+    }
+
+    private void addR(Node node, String word, int index) {
+        if (index == word.length()) {
+            if (!node.isWord) {
+                node.isWord = true;
+                size++;
+            }
+            return;
+        }
+
+        char c = word.charAt(index);
+        if (node.next.get(c) == null) {
+            node.next.put(c, new Node());
+        }
+        addR(node.next.get(c), word, index + 1);
+    }
+
+    public boolean contains(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (curr.next.get(c) == null) {
+                return false;
+            }
+            curr = curr.next.get(c);
+        }
+        return curr.isWord;
+    }
+
+    // 搜索--递归写法
+    public boolean containsR(String word) {
+        return containsR(root, word, 0);
+    }
+
+    private boolean containsR(Node node, String word, int index) {
+        if (index == word.length()) {
+            return node.isWord;
+        }
+
+        char c = word.charAt(index);
+        if (node.next.get(c) == null) {
+            return false;
+        }
+        return containsR(node.next.get(c), word, index + 1);
+    }
+
+    // 查询是否在trie中有单词以prefix为前缀的单词
+    public boolean isPrefix (String prefix) {
+        Node curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (curr.next.get(c) == null) {
+                return false;
+            }
+            curr = curr.next.get(c);
+        }
+        return true;
+    }
+
+    // 前缀搜索--递归写法
+    public boolean isPrefixR(String prefix) {
+        return isPrefixR(root, prefix, 0);
+    }
+
+    private boolean isPrefixR(Node node, String prefix, int index) {
+        if (index == prefix.length()) {
+            return true;
+        }
+
+        char c = prefix.charAt(index);
+        if (node.next.get(c) == null) {
+            return false;
+        }
+        return isPrefixR(node.next.get(c), prefix, index + 1);
+    }
+}
