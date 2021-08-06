f5a75810deed61e8a5067f5960a80db2bc7bb7ab
lichuangbo <15503609389@163.com>
2021-08-06 10:42:13 +0800

    438. 找到字符串中所有字母异位词(窗口)

--- src/top/xiaotian/algorithms/twoPointer/slidingWindow/FindAnagrams.java
+++ src/top/xiaotian/algorithms/twoPointer/slidingWindow/FindAnagrams.java
@@ -28,64 +28,102 @@ import java.util.List;
  * @created 2021/1/3
  */
 public class FindAnagrams {
+
+    /**
+     * 窗口移动
+     * 时间：频次统计O(pLen + (sLen-pLen+1)*pLen),滑动窗口O(sLen-pLen+1)
+     * 空间：频次数组O(26 + (sLen-pLen+1)*26)
+     */
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> resList = new ArrayList<>();
-        if (s.length() < p.length()) {
+        int sLen = s.length(), pLen = p.length();
+        if (sLen < pLen) {
             return resList;
         }
 
         // 统计p出现的频率
-        int[] freq = new int[26];
-        char[] pArr = p.toCharArray();
-        for (int i = 0; i < pArr.length; i++) {
-            freq[pArr[i] - 'a']++;
-        }
+        int[] freq = calcStrFreq(p);
 
-//        int l = 0, r = -1;// 滑动区间：[l...r]
-//        while (l < s.length()) {
-//            if (r + 1 < s.length() && r - l + 1 < p.length()) {
-//                r++;
-//            } else {
-//                l++;
-//            }
-//
-//            if (r - l + 1 == p.length() && isAnagrams(s.substring(l, r + 1), freq)) {
-//                resList.add(l);
-//            }
-//        }
+        // 1. 滑动窗口
+        int l = 0, r = -1;
+        while (l < sLen - pLen + 1) {
+            if (r + 1 < sLen && r - l + 1 < pLen) {
+                r++;
+            } else {
+                l++;
+            }
 
-        // 滑动窗口大小固定，耗时变长了
-        int l = 0, r = p.length() - 1;// 滑动区间：[l...r]
-        while (r < s.length()) {
-            if (isAnagrams(s.substring(l, r + 1), freq)) {
+            if (r - l + 1 == pLen && isAnagrams(s.substring(l, r + 1), freq)) {
                 resList.add(l);
             }
-            l++;
-            r++;
         }
+
+        // 2. 固定窗口
+//        int l = 0, r = p.length() - 1;// 滑动区间：[l...r]
+//        while (r < s.length()) {
+//            if (isAnagrams(s.substring(l, r + 1), freq)) {
+//                resList.add(l);
+//            }
+//            l++;
+//            r++;
+//        }
 
         return resList;
     }
 
-    // 判断两个字符串是否为互为字母异位词
+    // 判断判断字符串字母频次和给定频次是否一致
     private boolean isAnagrams(String a, int[] freq) {
-        char[] aArr = a.toCharArray();
-        int[] tmp = new int[26];
-        for (int i = 0; i < aArr.length; i++) {
-            tmp[aArr[i] - 'a']++;
-        }
+        int[] aFreq = calcStrFreq(a);
 
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] != tmp[i]) {
+        for (int i = 0; i < aFreq.length; i++) {
+            if (aFreq[i] != freq[i]) {
                 return false;
             }
         }
         return true;
     }
 
-    public static void main(String[] args) {
-        String s = "cbaebabacd", p = "abc";
-        List<Integer> anagrams = new FindAnagrams().findAnagrams(s, p);
-        System.out.println(anagrams);
+    // 计算字符串中字母出现的频率
+    private int[] calcStrFreq(String str) {
+        char[] strArr = str.toCharArray();
+        int[] tmp = new int[26];
+        for (int i = 0; i < strArr.length; i++) {
+            tmp[strArr[i] - 'a']++;
+        }
+        return tmp;
+    }
+
+    /**
+     * 时间：O(n)
+     * 空间：O(C)
+     */
+    public List<Integer> findAnagrams2(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        int sLen = s.length(), pLen = p.length();
+        if (sLen < pLen) {
+            return res;
+        }
+
+        // 频率统计
+        int[] sFreq = new int[26];
+        int[] pFreq = new int[26];
+        for (int i = 0; i < pLen; i++) {
+            sFreq[s.charAt(i) - 'a']++;
+            pFreq[p.charAt(i) - 'a']++;
+        }
+
+        for (int i = -1; i < sLen - pLen; i++) {
+            if (i >= 0) {
+                sFreq[s.charAt(i) - 'a']--;
+                sFreq[s.charAt(i + pLen) - 'a']++;
+            }
+            boolean isAnagrams = true;
+            for (int j = 0; j < 26; j++) {
+                isAnagrams &= sFreq[j] == pFreq[j];
+                if (!isAnagrams) break;
+            }
+            if (isAnagrams) res.add(i + 1);
+        }
+        return res;
     }
 }
