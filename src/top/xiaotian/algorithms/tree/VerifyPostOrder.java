a0fa413a136e3d27a7de4bb574dd6975947e8fcb
lichuangbo <15503609389@163.com>
2022-05-25 18:47:46 +0800

    二叉搜索树的后序遍历序列（递归，后序遍历）

--- src/top/xiaotian/algorithms/tree/VerifyPostOrder.java
+++ src/top/xiaotian/algorithms/tree/VerifyPostOrder.java
@@ -1,8 +1,5 @@
 package top.xiaotian.algorithms.tree;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 /**
  * 剑指 Offer 33. 二叉搜索树的后序遍历序列
  * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。
@@ -32,25 +29,31 @@ import java.util.LinkedList;
  * 数组长度 <= 1000
  */
 public class VerifyPostOrder {
+
   public boolean verifyPostorder(int[] postorder) {
-    // 单调栈使用，单调递增的单调栈
-    Deque<Integer> stack = new LinkedList<>();
-    int pervElem = Integer.MAX_VALUE;
-    // 逆向遍历，就是翻转的先序遍历
-    for (int i = postorder.length - 1; i >= 0; i--) {
-      // 左子树元素必须要小于递增栈被peek访问的元素，否则就不是二叉搜索树
-      if (postorder[i] > pervElem) {
+    return help(postorder, 0, postorder.length - 1);
+  }
+
+  // 判断postorder在[postSt, postEn]区间内表示的二插搜索树后序遍历顺序是否正确
+  boolean help(int[] postorder, int postSt, int postEn) {
+    // 仅有一个节点，判定为后序遍历顺序正确
+    if (postSt >= postEn) {
+      return true;
+    }
+
+    // 从postSt开始遍历，寻找第一个大于根节点的下标p
+    int rootVal = postorder[postEn];
+    int p = postSt;
+    while (postorder[p] < rootVal) {
+      p++;
+    }
+    // 右子树所有节点都必须大于根节点
+    for (int k = p; k <= postEn - 1; k++) {
+      if (postorder[k] < rootVal) {
         return false;
       }
-      while (!stack.isEmpty() && postorder[i] < stack.peek()) {
-        // 数组元素小于单调栈的元素了，表示往左子树走了，记录下上个根节点
-        // 找到这个左子树对应的根节点，之前右子树全部弹出，不再记录，因为不可能在往根节点的右子树走了
-        pervElem = stack.pop();
-      }
-      // 这个新元素入栈
-      stack.push(postorder[i]);
     }
-    return true;
+    return help(postorder, postSt, p - 1) && help(postorder, p, postEn - 1);
   }
 
 }
