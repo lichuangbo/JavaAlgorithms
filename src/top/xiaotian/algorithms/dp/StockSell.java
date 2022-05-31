87343c64d45af29407190e631074ae3e60e91bef
lichuangbo <15503609389@163.com>
2022-05-31 21:54:02 +0800

    买卖股票问题（动态规划）

--- src/top/xiaotian/algorithms/dp/StockSell.java
+++ src/top/xiaotian/algorithms/dp/StockSell.java
@@ -9,6 +9,55 @@ package top.xiaotian.algorithms.dp;
  */
 public class StockSell {
     /**
+     * 剑指 Offer 63. 股票的最大利润
+     * 假设把某股票的价格按照时间先后顺序存储在数组中，请问买卖该股票一次可能获得的最大利润是多少？
+     *
+     *
+     *
+     * 示例 1:
+     *
+     * 输入: [7,1,5,3,6,4]
+     * 输出: 5
+     * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+     * 示例 2:
+     *
+     * 输入: [7,6,4,3,1]
+     * 输出: 0
+     * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        // 状态定义：dp[i]表示以prices[i]为结尾的子数组的最大利润
+        // 状态转移方程：dp[i] = Math.max(dp[i - 1], prices[i] - min[0...i])
+        // 第i天的最大收益 = max (第i天不操作，取第i-1天的最大收益作为结果) (第i天卖出，但是想卖最好的价格，就得从数组中选出最小的元素)
+        int[] dp = new int[prices.length];
+        dp[0] = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            minPrice = Math.min(prices[i], minPrice);
+            dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);
+        }
+        return dp[prices.length - 1];
+    }
+
+    // 状态压缩
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int res = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            minPrice = Math.min(prices[i], minPrice);
+            res = Math.max(res, prices[i] - minPrice);
+        }
+        return res;
+    }
+
+    /**
      * 309. 最佳买卖股票时机含冷冻期
      * 给定一个整数数组，其中第 i 个元素代表了第 i 天的股票价格 。​
      *
@@ -24,7 +73,7 @@ public class StockSell {
      * @param prices
      * @return
      */
-    public int maxProfit(int[] prices) {
+    public int maxProfitII(int[] prices) {
         return 0;
     }
 }
