7f94de4f8f107fed0716bfd1975b0fd130de6307
lichuangbo <15503609389@163.com>
2022-06-11 17:09:17 +0800

    丑数（动态规划）

--- src/top/xiaotian/algorithms/dp/UglyNumber.java
+++ src/top/xiaotian/algorithms/dp/UglyNumber.java
@@ -1,10 +1,7 @@
-package top.xiaotian.dataStructures.heap.practice;
-
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Set;
+package top.xiaotian.algorithms.dp;
 
 /**
+ * 剑指 Offer 49. 丑数
  * 264. 丑数 II
  * 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
  *
@@ -29,6 +26,12 @@ import java.util.Set;
  */
 public class UglyNumber {
     public int nthUglyNumber(int n) {
+        /**
+         * 1              第一个丑数
+         * 2   3   5      第一轮比较，2最小，选2作为第二个丑数。p2+1=2
+         * 4   3   5      第二轮，只有p2发生了变化（那么3和5可以直接拿下来），他对应的值变为2*2=4。3最小，选3作为第三个丑数.p3+1=2
+         * 4   6   5      第三轮，同理只有p3发生了变化，对应的值变为2*3=6。4最小，选4作为第四个丑数，p2+1=3
+         */
         // dp[i]存储 第i个丑数
         int[] dp = new int[n + 1];
         // 初始值：第一个丑数为1
@@ -40,7 +43,7 @@ public class UglyNumber {
             int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
             // 三个丑数中最小的数 就是下一个要求解的丑数
             dp[i] = Math.min(num2, Math.min(num3, num5));
-            // 更新对应的指针
+            // 更新对应的指针  注意：丑数的值有可能是*2得来的，也有可能是*3得来的，这种情况下指针都需要移动
             if (dp[i] == num2) {
                 p2++;
             }
@@ -54,39 +57,10 @@ public class UglyNumber {
         return dp[n];
     }
 
-    public int nthUglyNumber2(int n) {
-        int[] factors = {2, 3, 5};
-        // 对堆中元素进行去重
-        Set<Long> set = new HashSet<>();
-        // 利用最小堆，堆顶元素是最小值的特性
-        PriorityQueue<Long> heap = new PriorityQueue<>();
-        set.add(1L);
-        heap.offer(1L);
-        int ugly = 0;
-        for (int i = 0; i < n; i++) {
-            // 第几次出堆，就是第几个丑数
-            long curr = heap.poll();
-            // 记录结果
-            ugly = (int) curr;
-            for (int factor : factors) {
-                long next = curr * factor;
-                // 堆中没有next值，就加入堆
-                if (!set.contains(next)) {
-                    heap.offer(next);
-                    set.add(next);
-                }
-            }
-        }
-        return ugly;
-    }
-
     // 263. 丑数
     public boolean isUgly(int n) {
         if (n <= 0) return false;
-        if (n == 1)  return true;
         // n = 2^a * 3^b * 5^c， abc都是非负整数
-        // 12 / 2 = 6   6 / 2 = 3   3 / 3 = 1  true
-        // 11 false
         while (n % 2 == 0) {
             n /= 2;
         }
@@ -98,6 +72,4 @@ public class UglyNumber {
         }
         return n == 1;
     }
-
-
 }
