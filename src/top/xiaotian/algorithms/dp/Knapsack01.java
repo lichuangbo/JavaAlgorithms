beac67b130889d7f333d86cc84e228954ddd9932
lichuangbo <15503609389@163.com>
2022-08-03 10:18:52 +0800

    01背包问题复习

--- src/top/xiaotian/algorithms/dp/Knapsack01.java
+++ src/top/xiaotian/algorithms/dp/Knapsack01.java
@@ -1,7 +1,7 @@
 package top.xiaotian.algorithms.dp;
 
 /**
- * 背包问题
+ * 01背包问题
  * @author lichuangbo
  * @email lichuangbo@smtp.telek.com.cn
  * @time 2021/2/9 16:35
@@ -9,15 +9,15 @@ package top.xiaotian.algorithms.dp;
  * 有一个背包，他的容量为C，现在有n种不同的物品，编号为0->n-1, 其中每一件物品的重量为w[i], 价值为v[i]
  * 请问：可以向这个背包中盛放那些物品，使得在不超过背包容量的基础上，物品的总价值最大
  */
-public class Knapsack {
-    private int[][] momo;
+public class Knapsack01 {
+    private int[][] memo;
     public int knapsack01(int[] w, int[] v, int C) {
         /**
          * 状态的定义： f(n, C)表示考虑将n个物品放进容量为C（物品放入背包后，背包容量会变小）的背包，使得背包中价值最大
          * f(i, c) = max(f(i-1, c), (v[i]+f(i-1, c-w[i]))   当前第i个物品不放背包，取前i-1个物品的价值   当前物品i放入背包，更新价值
          */
         int n = w.length;
-        momo = new int[n][C + 1];
+        memo = new int[n][C + 1];
         return bestValue(w, v, n - 1, C);
     }
     // 自顶向下： 用[0...index]编号的物品，填充容积为c的背包的最大价值
@@ -27,8 +27,8 @@ public class Knapsack {
             return 0;
         }
 
-        if (momo[index][c] != 0) {
-            return momo[index][c];
+        if (memo[index][c] != 0) {
+            return memo[index][c];
         }
         // 当前物品不放入背包
         int res = bestValue(w, v, index - 1, c);
@@ -36,28 +36,28 @@ public class Knapsack {
         if (c >= w[index]) {
             res = Math.max(res, v[index] + bestValue(w, v, index - 1, c - w[index]));
         }
-        momo[index][c] = res;
+        memo[index][c] = res;
         return res;
     }
 
     // 自底向上：
     public int knapsack01_dp(int[] w, int[] v, int C) {
         /**
-         * 三个物品 重量[1, 2, 3]  价值[6, 10, 12]  背包容量5
+         * 三个物品 重量[1, 2, 3]  价值[6, 10, 12]  背包容量5, 3*6的二维数组标识状态
          * 列：物品编号  行：背包容量
          *      0   1   2   3   4   5
-         *  0   0   6   6   6   6   6       第一行表示仅考虑0号物品 对应第一列表示：容量为0时任何物品都放不了，背包价值为0   对应第二列表示：容量为1时且仅考虑0号物品，那么他的背包价值为6  其他列虽然容量大于1，但是由于仅考虑0号物品，价值始终为6
-         *  1   0   6  10  16  16  16       第二行表示考虑0,1两个物品 对应第二列表示：容量为1只能放入0号物品，背包最大价值还为6  对应第三列表示：如果不考虑1号物品，那么他价值为6  如果考虑1号物品，1号物品重量为2，他要取(0, 2-2)的值再加上1号物品重量为10 最后取最大值为10
+         *  0   0   6   6   6   6   6       第一行表示仅考虑0号物品 对应第0列表示：容量为0时任何物品都放不了，背包价值为0   对应第1列表示：容量为1时且仅考虑0号物品，那么他的背包价值为6  其他列虽然容量大于1，但是由于仅考虑0号物品，价值始终为6
+         *  1   0   6  10  16  16  16       第二行表示考虑0,1两个物品 对应第1列表示：容量为1只能放入0号物品，背包最大价值还为6  对应第2列表示：如果不考虑1号物品，那么他价值为(0, 2)-6  如果考虑1号物品，1号物品重量为2，他要取(0, 2-2)的值再加上1号物品重量为10 最后取最大值为10
          *  2   0   6  10  16  18  22
          */
         int n = w.length;
         int[][] dp = new int[n][C + 1];
-        // 基础问题解决
+        // 初始化首行：当背包容量比第一个物品重量小初始化为0，否则初始化为第一个物品的体积
         for (int j = 0; j <= C; j++) {
             dp[0][j] = (j >= w[0]) ? v[0] : 0;
         }
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= C; j++) {
+        for (int i = 1; i < n; i++) {// 物品，行
+            for (int j = 0; j <= C; j++) {// 容量，列
                 // 不考虑当前物品
                 int res = dp[i - 1][j];
                 // 考虑当前物品
@@ -77,7 +77,7 @@ public class Knapsack {
     public int knapsack01_dp2(int[] w, int[] v, int C) {
         int n = w.length;
         int[][] dp = new int[2][C + 1];
-        // 基础问题解决
+        // 初始化
         for (int j = 0; j <= C; j++) {
             dp[0][j] = (j >= w[0]) ? v[0] : 0;
         }
@@ -102,7 +102,7 @@ public class Knapsack {
     public int knapsack01_dp3(int[] w, int[] v, int C) {
         int n = w.length;
         int[] dp = new int[C + 1];
-        // 基础问题解决
+        // 初始化
         for (int j = 0; j <= C; j++) {
             dp[j] = (j >= w[0]) ? v[0] : 0;
         }
@@ -112,14 +112,5 @@ public class Knapsack {
             }
         }
         return dp[C];
-    }
-
-    public static void main(String[] args) {
-        int[] w = {1, 2, 3};
-        int[] v = {6, 10, 12};
-        int C = 5;
-        Knapsack knapsack = new Knapsack();
-        int res = knapsack.knapsack01_dp3(w, v, C);
-        System.out.println(res);// 22
     }
 }
