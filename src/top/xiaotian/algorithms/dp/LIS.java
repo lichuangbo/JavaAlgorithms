7ac80233c09ed830631e0ab4dcf2c0e942cdbdfb
lichuangbo <15503609389@163.com>
2022-08-27 22:51:22 +0800

    最长递增子序列（动态规划）

--- src/top/xiaotian/algorithms/dp/LIS.java
+++ src/top/xiaotian/algorithms/dp/LIS.java
@@ -4,49 +4,42 @@ import java.util.Arrays;
 
 /**
  * 300. 最长递增子序列
- 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
-
- 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
-
-
- 示例 1：
-
- 输入：nums = [10,9,2,5,3,7,101,18]
- 输出：4
- 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * <p>
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+ *
  * @author lichuangbo
  * @version 1.0
  * @created 2021/3/17  376
  */
 public class LIS {
-    public int lengthOfLIS(int[] nums) {
-        // dp[i]表示以第i个数字为结尾的最长上升子序列的长度
-        // 在[0...i]的范围内，选择数字nums[i]可以获得的最长上升子序列的长度
-        int[] dp = new int[nums.length];
-        /**
-         * 10, 9, 2, 5, 3, 7, 101, 18
-         * 1   1  1  1  1  1   1   1    初始状态：不组合，仅自己
-         * 1   1  1  2 => 5比10、9小,不更新，比2大更新为dp[2]+1
-         */
-        Arrays.fill(dp, 1);
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
+  public int lengthOfLIS(int[] nums) {
+    int len = nums.length;
+    // dp[i]表示以nums[i]结尾的序列的最大长度
+    int[] dp = new int[len];
+    // 初始化：1，每一个数字都可以作为递增子序列的最大长度
+    Arrays.fill(dp, 1);
+    for (int i = 1; i < len; i++) {// 处理每个状态
+      for (int j = 0; j < i; j++) {// 找到比nums[i]小的数字，做状态的更新
+        if (nums[j] < nums[i]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+          // 放到这里也是ok的
+          // res = Math.max(res, dp[i]);
         }
-        int res = 1;
-        for (int i = 0; i < dp.length; i++) {
-            res = Math.max(res, dp[i]);
-        }
-        return res;
+      }
     }
-
-    public static void main(String[] args) {
-        LIS lis = new LIS();
-        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
-        int res = lis.lengthOfLIS(nums);
-        System.out.println(res);
+    // 由于dp[i]的含义，需要遍历一遍dp数组，才能得到全局最大长度
+    int res = 1;
+    for (int i = 0; i < len; i++) {
+      res = Math.max(res, dp[i]);
     }
+    return res;
+  }
 }
