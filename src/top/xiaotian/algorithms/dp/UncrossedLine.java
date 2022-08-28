7468cd07dca0ddc527f52a0b2bba429ace1815d6
lichuangbo <15503609389@163.com>
2022-08-28 10:10:35 +0800

    不相交的线（LCS变形、动态规划）

---
+++ src/top/xiaotian/algorithms/dp/UncrossedLine.java
@@ -0,0 +1,40 @@
+package top.xiaotian.algorithms.dp;
+
+/**
+ * 1035. 不相交的线
+ * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+ *
+ * 现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足满足：
+ *
+ *  nums1[i] == nums2[j]
+ * 且绘制的直线不与任何其他连线（非水平线）相交。
+ * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+ *
+ * 以这种方法绘制线条，并返回可以绘制的最大连线数。
+ *
+ * 示例 1：
+ * 输入：nums1 = [1,4,2], nums2 = [1,2,4]
+ * 输出：2
+ * 解释：可以画出两条不交叉的线，如上图所示。
+ * 但无法画出第三条不相交的直线，因为从 nums1[1]=4 到 nums2[2]=4 的直线将与从 nums1[2]=2 到 nums2[1]=2 的直线相交。
+ */
+public class UncrossedLine {
+  public int maxUncrossedLines(int[] nums1, int[] nums2) {
+    // 直线不能相交，这就是说明在字符串A中 找到一个与字符串B相同的子序列，且这个子序列不能改变相对顺序，只要相对顺序不改变，链接相同数字的直线就不会相交
+    // 问题转化为：求两个字符串的最长公共子序列的长度
+    // dp[i][j]表示以nums1[i]结尾和以nums2[j]结尾的字符串的最长公共子序列的长度
+    int len1 = nums1.length;
+    int len2 = nums2.length;
+    int[][] dp = new int[len1 + 1][len2 + 1];
+    for (int i = 1; i <= len1; i++) {
+      for (int j = 1; j <= len2; j++) {
+        if (nums1[i - 1] == nums2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+    return dp[len1][len2];
+  }
+}
