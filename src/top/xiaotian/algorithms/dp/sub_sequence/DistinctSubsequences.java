d30bd6050511a2cc50a7e6c27fb111c85dc3a03f
lichuangbo <15503609389@163.com>
2022-08-31 23:09:05 +0800

    不同的子序列（动态规划）

--- src/top/xiaotian/algorithms/dp/sub_sequence/DistinctSubsequences.java
+++ src/top/xiaotian/algorithms/dp/sub_sequence/DistinctSubsequences.java
@@ -1,4 +1,4 @@
-package top.xiaotian.algorithms.dp;
+package top.xiaotian.algorithms.dp.sub_sequence;
 
 import java.util.Arrays;
 
@@ -23,29 +23,28 @@ import java.util.Arrays;
  * ^^ ^^^^
  * rabbbit
  * ^^^ ^^^
- * @author lichuangbo
- * @version 1.0
- * @created 2021/3/17
  */
-public class NumDistinct {
-    private int[][] mono;
+public class DistinctSubsequences {
+    private int[][] memo;
     public int numDistinct(String s, String t) {
         /**
          * s:babgbag    最后一个字符相同，有两种选择：一使用s[i]匹配掉t[j]  二不用s[i]去匹配t[j]，使用i之前的字符去匹配
          * t:bag        结果为两种选择之和
          *              如果字符不相同，只能使用s的前i-1个字符去匹配t，因为t是不能变的
          */
-        mono = new int[s.length()][t.length()];
-        for (int i = 0; i < mono.length; i++) {
-            for (int j = 0; j < mono[0].length; j++) {
-                mono[i][j] = -1;
-            }
+        int slen = s.length();
+        int tLen = t.length();
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+        memo = new int[slen][tLen];
+        for (int i = 0; i < slen; i++) {
+            Arrays.fill(memo[i], -1);
         }
-        return help(s, s.length() - 1, t, t.length() - 1);
+        return help(chars1, slen - 1, chars2, tLen - 1);
     }
     // 返回从开头到s[i]的子串中，出现『从开头到t[j]的子串』的次数
-    private int help(String s, int i, String t, int j) {
-        // t变成空串，不管s有多少个字符，都只有一种匹配方法--删除s的所有字符
+    private int help(char[] chars1, int i, char[] chars2, int j) {
+        // t变成空串，说明s是一路匹配过来的，算一种解法
         if (j < 0) {
             return 1;
         }
@@ -54,53 +53,36 @@ public class NumDistinct {
             return 0;
         }
 
-        if (mono[i][j] != -1) {
-            return mono[i][j];
+        if (memo[i][j] != -1) {
+            return memo[i][j];
         }
-        if (s.charAt(i) == t.charAt(j)) {
-            mono[i][j] = help(s, i - 1, t, j - 1) + help(s, i - 1, t, j);
+        if (chars1[i] == chars2[j]) {
+            memo[i][j] = help(chars1, i - 1, chars2, j - 1) + help(chars1, i - 1, chars2, j);
         } else {
-            mono[i][j] = help(s, i - 1, t, j);
+            memo[i][j] = help(chars1, i - 1, chars2, j);
         }
-        return mono[i][j];
+        return memo[i][j];
     }
 
     public int numDistinct2(String s, String t) {
         int sLen = s.length(), tLen = t.length();
-        // dp状态：以i-1为结尾的s子串中出现以j-1为结尾的t子串的个数
+        // dp状态：以i-1为结尾的s子串中出现以j-1为结尾的t字符串的个数
         int[][] dp = new int[sLen + 1][tLen + 1];
+        // 初始化：dp[i][0]表示s子串可以任意删除字符去匹配t空子串，1    dp[0][j]表示s子串为空，但是t子串不空，无法操作0
         for (int i = 0; i <= sLen; i++) {
             dp[i][0] = 1;
         }
-        /**
-         * dp[i][0]表示s子串可以任意删除字符去匹配t空子串，1
-         * dp[0][j]表示s子串为空，但是t子串不空，无法操作0
-         * [1, 0, 0, 0, 0, 0, 0],
-         * [1, 1, 0, 0, 0, 0, 0],
-         * [1, 1, 1, 0, 0, 0, 0],
-         * [1, 1, 1, 1, 0, 0, 0],
-         * [1, 1, 1, 2, 1, 0, 0],
-         * [1, 1, 1, 3, 3, 0, 0],
-         * [1, 1, 1, 3, 3, 3, 0],
-         * [1, 1, 1, 3, 3, 3, 3]
-         */
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
         for (int i = 1; i <= sLen; i++) {
             for (int j = 1; j <= tLen; j++) {
-                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                if (chars1[i - 1] == chars2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        System.out.println(Arrays.deepToString(dp));
         return dp[sLen][tLen];
-    }
-
-    public static void main(String[] args) {
-        String s = "rabbbit", t = "rabbit";
-        NumDistinct numDistinct = new NumDistinct();
-        int res = numDistinct.numDistinct2(s, t);
-        System.out.println(res);
     }
 }
