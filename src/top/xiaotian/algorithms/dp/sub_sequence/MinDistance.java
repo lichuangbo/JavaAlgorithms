a1aad933777f87c3f90a6abeddea57fbbb598920
lichuangbo <15503609389@163.com>
2022-09-01 21:34:01 +0800

    两个字符串的删除操作（动态规划）

---
+++ src/top/xiaotian/algorithms/dp/sub_sequence/MinDistance.java
@@ -0,0 +1,43 @@
+package top.xiaotian.algorithms.dp.sub_sequence;
+
+/**
+ * 583. 两个字符串的删除操作
+ * 给定两个单词 word1 和 word2 ，返回使得 word1 和  word2 相同所需的最小步数。
+ * <p>
+ * 每步 可以删除任意一个字符串中的一个字符。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: word1 = "sea", word2 = "eat"
+ * 输出: 2
+ * 解释: 第一步将 "sea" 变为 "ea" ，第二步将 "eat "变为 "ea"
+ */
+public class MinDistance {
+  public int minDistance(String word1, String word2) {
+    int len1 = word1.length();
+    int len2 = word2.length();
+    char[] chars1 = word1.toCharArray();
+    char[] chars2 = word2.toCharArray();
+    // dp[i][j]表示以chars1[i-1]结尾，chars2[j-1]结尾的两个字符串，要达到相等需要的最小步数
+    int[][] dp = new int[len1 + 1][len2 + 1];
+    // 初始化：不管哪一个是空串，想要匹配都需要把另一个字符串全部删完
+    for (int i = 1; i <= len1; i++) {
+      dp[i][0] = i;
+    }
+    for (int j = 1; j <= len2; j++) {
+      dp[0][j] = j;
+    }
+    for (int i = 1; i <= len1; i++) {
+      for (int j = 1; j <= len2; j++) {
+        if (chars1[i - 1] == chars2[j - 1]) {// 相等，直接取子串，因为并没有任何删除操作
+          dp[i][j] = dp[i - 1][j - 1];
+        } else {// 不相等：删除w1[i]w2[j]再去匹配，删除w1[i]用w1去匹配w2, 删除w2[j]用w2去匹配w1
+          dp[i][j] = Math.min(dp[i - 1][j - 1] + 2, Math.min(dp[i][j - 1], dp[i - 1][j]) + 1);
+        }
+      }
+    }
+    return dp[len1][len2];
+  }
+}
