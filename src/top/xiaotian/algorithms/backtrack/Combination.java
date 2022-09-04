9e1cdf99567c54b04c4554c91c5a2115c6ec025e
lichuangbo <15503609389@163.com>
2022-09-04 10:40:16 +0800

    组合/组合总和 III（回溯）

--- src/top/xiaotian/algorithms/backtrack/Combination.java
+++ src/top/xiaotian/algorithms/backtrack/Combination.java
@@ -6,32 +6,35 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 77. 组合
- * 给定两个整数 rows 和 k，返回 1 ... rows 中所有可能的 k 个数的组合。
- *
- * 示例:
- *
- * 输入: rows = 4, k = 2
- * 输出:
- * [
- *   [2,4],
- *   [3,4],
- *   [2,3],
- *   [1,2],
- *   [1,3],
- *   [1,4],
- * ]
- * @author lichuangbo
- * @email lichuangbo@smtp.telek.com.cn
- * @time 2021/1/29 9:45
- * @Description: 描述: 39 40 216 78 90 401
+ * 组合问题
  */
 public class Combination {
     private List<List<Integer>> res;
 
+    /**
+     * 77. 组合
+     * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+     *
+     * 你可以按 任何顺序 返回答案。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：n = 4, k = 2
+     * 输出：
+     * [
+     *   [2,4],
+     *   [3,4],
+     *   [2,3],
+     *   [1,2],
+     *   [1,3],
+     *   [1,4],
+     * ]
+     */
     public List<List<Integer>> combine(int n, int k) {
         res = new ArrayList<>();
-        if (n <= 0 || k <= 0 || k > n) {
+        if (k <= 0 || k > n) {
             return res;
         }
 
@@ -39,7 +42,7 @@ public class Combination {
         return res;
     }
 
-    // 求解C(rows, k), 当前已经找到的组合存储在curr中，需要从start开始搜索新的元素
+    // 求解C(n, k), 当前已经找到的组合存储在curr中，需要从start开始搜索新的元素
     private void generateCombinations(int n, int k, int start, LinkedList<Integer> curr) {
         // 已经凑齐k个数，说明找到了一组解
         if (curr.size() == k) {
@@ -48,11 +51,12 @@ public class Combination {
         }
 
         /**
-         * 剪枝：现在i遍历区间[start...rows]，但是当i=n时，在循环里继续递归还会+1，此时已经没有空位可选了
-         * curr中存储着已经找到的元素，说明还有k-curr.size()个空位可以排列，i取多少，可以确保[i...rows]区间中至少有k-curr.size个元素
+         * 剪枝：现在i遍历区间[start...n]，但是当i=n时，在循环里继续递归还会+1，此时已经没有空位可选了
+         * curr中存储着已经找到的元素，说明还有k-curr.size()个空位可以排列，i取多少，可以确保[i...n]区间中至少有k-curr.size个元素
          * 2个空位：k-curr.size=2,i应该取n-1       1个空位：k-curr.size=1,i应该取n
-         * 观察：i最多取 rows - (k - curr.size) + 1
+         * 观察：i最多取 n - (k - curr.size) + 1
          */
+//        for (int i = start; i <= n; i++) {
         for (int i = start; i <= n - (k - curr.size()) + 1; i++) {
             curr.addLast(i);
             generateCombinations(n, k, i + 1, curr);
@@ -78,9 +82,6 @@ public class Combination {
      *   [7],
      *   [2,2,3]
      * ]
-     * @param candidates
-     * @param target
-     * @return
      */
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         res = new ArrayList<>();
@@ -153,34 +154,29 @@ public class Combination {
 
     /**
      * 216. 组合总和 III
-     * 找出所有相加之和为 rows 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
+     * 找出所有相加之和为 n 的 k 个数的组合，且满足下列条件：
      *
-     * 说明：
+     * 只使用数字1到9
+     * 每个数字 最多使用一次
+     * 返回 所有可能的有效组合的列表 。该列表不能包含相同的组合两次，组合可以以任何顺序返回。
      *
-     * 所有数字都是正整数。
-     * 解集不能包含重复的组合。
+     *
+     *
      * 示例 1:
      *
-     * 输入: k = 3, rows = 7
+     * 输入: k = 3, n = 7
      * 输出: [[1,2,4]]
-     * @param k
-     * @param n
-     * @return
+     * 解释:
+     * 1 + 2 + 4 = 7
+     * 没有其他符合的组合了。
      */
-    public List<List<Integer>> combinationSum3(int k, int n) {
+    public List<List<Integer>> combinationSumIII(int k, int n) {
         res = new ArrayList<>();
-        if (k == 0) {
-            return res;
-        }
-
         generateSumIII(k, n, 1, new LinkedList<>());
         return res;
     }
 
     private void generateSumIII(int k, int n, int start, LinkedList<Integer> curr) {
-        if (n < 0) {
-            return;
-        }
         if (curr.size() == k && n == 0) {
             res.add(new ArrayList<>(curr));
             return;
@@ -191,14 +187,5 @@ public class Combination {
             generateSumIII(k, n - i, i + 1, curr);
             curr.removeLast();
         }
-    }
-
-    public static void main(String[] args) {
-        int[] candidates = {10, 1, 2, 7, 6, 1, 5};
-        Combination combination = new Combination();
-        List<List<Integer>> res = combination.combinationSum2(candidates, 8);
-
-//        List<List<Integer>> res = combination.combinationSum3(3, 7);
-        System.out.println(res);
     }
 }
