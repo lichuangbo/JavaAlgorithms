205a73f52e607333aadea51764039c320737cea0
lichuangbo <15503609389@163.com>
2022-09-13 12:22:50 +0800

    下一个更大元素 II(单调栈)

--- src/top/xiaotian/algorithms/stack/monotone_stack/NextGreaterElement.java
+++ src/top/xiaotian/algorithms/stack/monotone_stack/NextGreaterElement.java
@@ -63,4 +63,33 @@ public class NextGreaterElement {
     return res;
   }
 
+  /**
+   * 503. 下一个更大元素 II 给定一个循环数组 nums （ nums[nums.length - 1] 的下一个元素是 nums[0] ），返回 nums 中每个元素的 下一个更大元素
+   * 。
+   *
+   * 数字 x 的 下一个更大的元素 是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。如果不存在，则输出 -1 。
+   *
+   *
+   *
+   * 示例 1:
+   *
+   * 输入: nums = [1,2,1] 输出: [2,-1,2] 解释: 第一个 1 的下一个更大的数是 2； 数字 2 找不到下一个更大的数； 第二个 1
+   * 的下一个最大的数需要循环搜索，结果也是 2。
+   */
+  public int[] nextGreaterElements(int[] nums) {
+    int len = nums.length;
+    int[] res = new int[len];
+    Arrays.fill(res, -1);
+    Deque<Integer> deque = new ArrayDeque<>();
+    // 滚动数组，只要遍历两遍就可以和 每日温度 处理思路一致
+    for (int i = 0; i < len * 2; i++) {
+      while (!deque.isEmpty() && nums[deque.peekLast() % len] < nums[i % len]) {
+        Integer index = deque.pollLast();
+        res[index] = nums[i % len];
+      }
+      deque.addLast(i % len);
+    }
+    return res;
+  }
+
 }
