6b8e4712805039be0a797008092cb333d6f68af6
lichuangbo <15503609389@163.com>
2022-10-05 10:37:42 +0800

    回文链表（链表）

--- src/top/xiaotian/dataStructures/linkedlist/practice/Palindrome4LinkedNode.java
+++ src/top/xiaotian/dataStructures/linkedlist/practice/Palindrome4LinkedNode.java
@@ -1,46 +1,46 @@
 package top.xiaotian.dataStructures.linkedlist.practice;
 
+import top.xiaotian.util.ListNode;
+
 /**
- * 判断一个字符串是否是回文字符串(其中字符串使用链表存储)
+ * 234. 回文链表
+ * 面试题 02.06. 回文链表
+ * 编写一个函数，检查输入的链表是否是回文的。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入： 1->2
+ * 输出： false
  * @author lichuangbo
  * @version 1.0
  * @created 2020/12/5
  */
 public class Palindrome4LinkedNode {
-    public static boolean isPalindromeStr(Node head) {
-        Node fast = head;
-        Node slow = head;
-        Node prev = null;// 翻转链表用
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
-            // 寻找中点的同时，进行链表反转
-            Node next = slow.next;
+            // 翻转
+            ListNode next = slow.next;
             slow.next = prev;
             prev = slow;
+
             slow = next;
         }
-        // 处理奇数个节点情况，让slow再走一步
-        if (fast != null) {
+        if (fast != null) {// 奇数个节点
             slow = slow.next;
         }
-        // 判断
-        while (slow != null) {
-            if (!slow.value.equals(prev.value)) {
+        while (prev != null && slow != null) {
+            if (prev.val != slow.val) {
                 return false;
             }
-            slow = slow.next;
             prev = prev.next;
+            slow = slow.next;
         }
         return true;
-    }
-
-    public static void main(String[] args) {
-        Node head1 = new Node("1", new Node("2", new Node("3", new Node("2", new Node("1", null)))));
-        Node head2 = new Node("1", new Node("2", new Node("2", new Node("1", null))));
-        Node head3 = new Node("1", new Node("2", new Node("3", new Node("1", null))));
-
-        System.out.println(isPalindromeStr(head1));
-        System.out.println(isPalindromeStr(head2));
-        System.out.println(isPalindromeStr(head3));
     }
 }
