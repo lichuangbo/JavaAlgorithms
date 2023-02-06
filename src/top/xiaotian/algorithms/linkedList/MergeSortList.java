e42b4bcc07b3c44bdcd05379bb0a19864c00cc7b
lichuangbo <15503609389@163.com>
2023-02-06 18:08:21 +0800

    合并K个升序链表(分治)

--- src/top/xiaotian/algorithms/linkedList/MergeSortList.java
+++ src/top/xiaotian/algorithms/linkedList/MergeSortList.java
@@ -55,4 +55,22 @@ public class MergeSortList {
     }
     return dummyHead.next;
   }
+
+  // 递归
+  private ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+    if (l1 == null) {
+      return l2;
+    }
+    if (l2 == null) {
+      return l1;
+    }
+
+    if (l1.val < l2.val) {
+      l1.next = mergeTwoLists2(l1.next, l2);
+      return l1;
+    } else {
+      l2.next = mergeTwoLists2(l1, l2.next);
+      return l2;
+    }
+  }
 }
