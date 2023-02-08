fc145cc82d22be561b8695ba77f52180ae44dcc0
lichuangbo <15503609389@163.com>
2023-02-08 22:51:47 +0800

    冒泡、插入、选择排序分析

--- src/top/xiaotian/algorithms/sort/SelectionSort.java
+++ src/top/xiaotian/algorithms/sort/SelectionSort.java
@@ -1,10 +1,5 @@
 package top.xiaotian.algorithms.sort;
 
-import top.xiaotian.util.RandomUtil;
-import top.xiaotian.util.SwapUtil;
-
-import java.util.Arrays;
-
 /**
  * 选择排序
  * @author lichuangbo
@@ -13,29 +8,25 @@ import java.util.Arrays;
  * @Description: 描述:
  */
 public class SelectionSort {
-    /***
-     * 时间O(n2)
-     * 空间O(1)
-     * @param arr
-     */
-    public void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int minIndex = i;// 记录数组中最小值对应的下标
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            // 找到最小值，即交换位置
-            SwapUtil.swap(arr, i, minIndex);
+  /***
+   * 时间O(n2)
+   * 空间O(1)
+   * 不稳定排序算法：每次在无序集中查找最小值的做法破坏了稳定性
+   * @param arr
+   */
+  public void selectionSort(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      int minIndex = i;// 记录数组中最小值对应的下标
+      // i索引前边都是有序的，从i索引以后结果集找出最小值
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
         }
+      }
+      // 每一趟都能找到一个最小值，找到就交换位置
+      int tmp = arr[minIndex];
+      arr[minIndex] = arr[i];
+      arr[i] = tmp;
     }
-
-    public static void main(String[] args) {
-        int[] arr = RandomUtil.randomInt(10000, 1, 10000);
-
-        SelectionSort sort = new SelectionSort();
-        sort.selectionSort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
+  }
 }
