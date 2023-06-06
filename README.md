de5abdef1c22a468b71ded65c9dd8e43c4a0b327
lichuangbo <15503609389@163.com>
2023-06-06 14:51:49 +0800

    无重复字符的最长子串（滑动窗口）

--- README.md
+++ README.md
@@ -116,7 +116,9 @@
 * [581. 最短无序连续子数组](src/top/xiaotian/algorithms/twoPointer/collisionPointer/UnsortedSubarray.java)
 
 ##### 滑动窗口
-* [581. 最短无序连续子数组](src/top/xiaotian/algorithms/twoPointer/slidingWindow/FindAnagrams.java)
+* [438. 找到字符串中所有字母异位词](src/top/xiaotian/algorithms/twoPointer/slidingWindow/FindAnagrams.java)
+
+* [3. 无重复字符的最长子串](src/top/xiaotian/algorithms/twoPointer/slidingWindow/LongestSubstring.java)
 
 #### 查找表法
 * [560. 和为 K 的子数组](src/top/xiaotian/algorithms/map/SubarraySum.java)
--- src/top/xiaotian/algorithms/twoPointer/slidingWindow/LongestSubstring.java
+++ src/top/xiaotian/algorithms/twoPointer/slidingWindow/LongestSubstring.java
@@ -1,18 +1,35 @@
 package top.xiaotian.algorithms.twoPointer.slidingWindow;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * 剑指 Offer 48. 最长不含重复字符的子字符串
  * 3. 无重复字符的最长子串
- * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。(s由英文字母、数字、符号和空格组成)
- *
- * 示例 1:
- *
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
  * 输入: s = "abcabcbb"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * <p>
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= s.length <= 5 * 104
+ * s 由英文字母、数字、符号和空格组成
+ *
  * @author lichuangbo
  * @version 1.0
  * @created 2021/1/3  438 76
@@ -21,12 +38,10 @@ public class LongestSubstring {
     /**
      * 滑动窗口: 滑动窗口走一遍，同时用变量存储最大窗口值
      * 时间O(n)
-     * @param s
-     * @return
      */
     public int lengthOfLongestSubstring(String s) {
-        // 记录元素是否重复，0是 1否
         int[] freq = new int[256];
+        // 滑动窗口左闭右闭，初始大小为0
         int l = 0, r = -1;
         int res = 0;
         char[] chars = s.toCharArray();
@@ -34,7 +49,7 @@ public class LongestSubstring {
             if (r + 1 < chars.length && freq[chars[r + 1]] == 0) {// 右边界+1位置元素与滑动窗口内的元素不重复
                 r++;
                 freq[chars[r]]++;
-            } else {
+            } else {// 出现重复，缩小窗口
                 freq[chars[l]]--;
                 l++;
             }
@@ -45,22 +60,19 @@ public class LongestSubstring {
     }
 
     public int lengthOfLongestSubstring2(String s) {
-        // 划定当前窗口的坐标为(start,i],左开右闭
-        int max = 0, start = -1;
-        // 使用哈希表map统计各字符最后一次出现的索引位置
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char tmp = s.charAt(i);
-
-            // 当字符在map中已经存储时，需要判断其索引值index和当前窗口start的大小以确定是否需要对start进行更新:
-            // 当index>start时，start更新为当前的index,否则保持不变
-            if (map.containsKey(tmp)) {
-                start = Math.max(start, map.get(tmp));
+        int[] freq = new int[256];
+        char[] chars = s.toCharArray();
+        int res = 0;
+        for (int l = 0, r = 0; r < chars.length; r++) {
+            freq[chars[r]]++;
+            // while控制chars[r]没有出现重复  比如pww，当r指向2时，l移动一次是不行的，ww仍然是重复字符
+            while (freq[chars[r]] > 1) {
+                freq[chars[l]]--;
+                l++;
             }
-            map.put(tmp, i);
 
-            max = Math.max(max, i - start);
+            res = Math.max(res, r - l + 1);
         }
-        return max;
+        return res;
     }
 }
