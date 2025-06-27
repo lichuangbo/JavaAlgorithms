71e971ba174a18926dc8dc3c832cfceb45e5c6fa
lichuangbo <15503609389@163.com>
2025-06-27 14:10:28 +0800

    题目整理

--- src/top/xiaotian/algorithms/dp/knapsack01/OneAndZeroes.java
+++ src/top/xiaotian/algorithms/dp/knapsack01/OneAndZeroes.java
@@ -31,54 +31,65 @@ package top.xiaotian.algorithms.dp.knapsack01;
  * 1 <= m, n <= 100
  */
 public class OneAndZeroes {
-  public int findMaxForm(String[] strs, int m, int n) {
-    /**
-     * m个0，n个1可以理解为背包容量的两个维度，物品是字符串数组strs
-     * dp[i][j][k] 表示输入字符串在子区间 [0, i] 能够使用 j 个 0 和 k 个 1 的字符串的最大数量
-     */
-    int len = strs.length;
-    int[][][] dp = new int[len + 1][m + 1][n + 1];
-    // 首行初始化为0
-    for (int i = 1; i <= len; i++) {
-      // 注意：有一位偏移
-      int[] count = countZeroAndOne(strs[i - 1]);
-      for (int j = 0; j <= m; j++) {
-        for (int k = 0; k <= n; k++) {
-          // 先把上一行抄下来
-          dp[i][j][k] = dp[i - 1][j][k];
-          int zeros = count[0];
-          int ones = count[1];
-          if (j >= zeros && k >= ones) {
-            dp[i][j][k] = Math.max(dp[i - 1][j][k], dp[i - 1][j - zeros][k - ones] + 1);
-          }
+    public int findMaxForm(String[] strs, int m, int n) {
+        /**
+         * m个0，n个1可以理解为背包容量的两个维度，物品是字符串数组strs
+         * dp[i][j][k] 表示输入字符串在子区间 [0, i] 能够使用 j 个 0 和 k 个 1 的字符串的最大数量
+         */
+        int len = strs.length;
+        int[][][] dp = new int[len + 1][m + 1][n + 1];
+        // 首行初始化为0
+        for (int i = 1; i <= len; i++) {
+            // 注意：有一位偏移
+            int[] count = countZeroAndOne(strs[i - 1]);
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    // 先把上一行抄下来
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    int zeros = count[0];
+                    int ones = count[1];
+                    if (j >= zeros && k >= ones) {
+                        dp[i][j][k] = Math.max(dp[i - 1][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                    }
+                }
+            }
         }
-      }
+        return dp[len][m][n];
     }
-    return dp[len][m][n];
-  }
 
-  private int[] countZeroAndOne(String str) {
-    int[] cnt = new int[2];
-    for (char c : str.toCharArray()) {
-      cnt[c - '0']++;
-    }
-    return cnt;
-  }
-
-  public int findMaxForm2(String[] strs, int m, int n) {
-    // 状态压缩
-    int[][] dp = new int[m + 1][n + 1];
-    dp[0][0] = 0;
-    for (String s : strs) {
-      int[] zeroAndOne = countZeroAndOne(s);
-      int zeros = zeroAndOne[0];
-      int ones = zeroAndOne[1];
-      for (int i = m; i >= zeros; i--) {
-        for (int j = n; j >= ones; j--) {
-          dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+    private int[] countZeroAndOne(String str) {
+        int[] cnt = new int[2];
+        for (char c : str.toCharArray()) {
+            cnt[c - '0']++;
         }
-      }
+        return cnt;
     }
-    return dp[m][n];
-  }
+
+    public int findMaxForm2(String[] strs, int m, int n) {
+        // 转化为背包问题，strs字符串数组是物品，成本是0和1的数量，价值是纳入这个字符串1
+        // dp[i][j]表示使用最多i个0 j个1时，能组成得最大子集大小
+        // 针对当前字符，选择纳入      状态从dp[i-zeros][j-ones]推演过来，再加上价值1(其中zeros是这个字符串中0的数量，ones是这个字符串中1的数量)
+        //             选择不纳入    状态依旧保持dp[i][j]
+        int[][] dp = new int[m + 1][n + 1];
+        // 初始化
+        dp[0][0] = 0;
+
+        for (String str : strs) {
+            int ones = 0, zeros = 0;
+            for (char c : str.toCharArray()) {
+                if (c == '0') {
+                    zeros++;
+                } else {
+                    ones++;
+                }
+            }
+
+            for (int i = m; i >= zeros; i--) {
+                for (int j = n; j >= ones; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
 }
